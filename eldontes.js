70f71e53f3369302d992b4e15461579701999f1c
Tóth Máté <mate.toth@flowacademy.hu>
2018-02-07 16:55:48 +0100

    szerdaeste2

--- eldontes.js
+++ eldontes.js
@@ -1,11 +1,11 @@
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let ker = 5;
 let i = 0;
-while (i < array.length && array[i] != ker) {
-    i += 1;
+while (i < array.length && array[i] !== ker) {
+  i += 1;
 }
 if (i < array.length) {
-    console.log("Megtalálva!");
+  console.log('Megtalálva!');
 } else {
-    console.log("Nincs benne!");
+  console.log('Nincs benne!');
 }
--- szetvalogatas.js
+++ szetvalogatas.js
@@ -1,17 +1,17 @@
 let j = 0;
 let k = 0;
-let innen = [1, 2, -3, -4, 5, -6, 7, -8, 9, -10];
-let ide = [];
-let is = [];
+let innen = [1, -2, -3, -4, 5, -6, 7, -8, 9, -10];
+let pozitiv = [];
+let negativ = [];
 for (let i = 0; i < innen.length; i++) {
   if (innen[i] < 0) {
-    ide[j] = innen[i];
+    negativ[j] = innen[i];
     j += 1;
   } else {
-    is[k] = innen[i];
+    pozitiv[k] = innen[i];
     k += 1;
   }
 }
 
-console.log(is);
-console.log(ide);
+console.log(pozitiv);
+console.log(negativ);
